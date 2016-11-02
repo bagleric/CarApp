7112ea3207c2dde030fe90d1a25897882181eee7
rol09003 <rol09003@byui.edu>
2016-11-01 18:24:32 -0600

    carApp main

--- app/src/main/java/com/example/carapp/MainActivity.java
+++ app/src/main/java/com/example/carapp/MainActivity.java
@@ -1,19 +1,55 @@
 package com.example.carapp;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
+    private int oilMiles;
+    private int tireMiles;
+    private int driverMiles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Mariah's comment
+    }
 
-        //conflict issue comment
-        //Mikes comment
-        //Eric's Comments again
+
+    public int getOilMiles() {
+        return oilMiles;
+    }
+
+    public int getTireMiles() {
+        return tireMiles;
+    }
+
+    public int getDriverMiles() {
+        return driverMiles;
+    }
+
+    public void setOilMiles(int Omiles) {
+        this.oilMiles = Omiles;
+    }
+
+    public void setTireMiles(int Tmiles) {
+        this.tireMiles = Tmiles;
+    }
+
+    public void setDriverMiles(int Dmiles) {
+        this.driverMiles = Dmiles;
+    }
+
+    public Calendar getCurrentDate(){
+        String currentDate;
+        DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+        Calendar calobj = Calendar.getInstance();
+        System.out.println(df.format(calobj.getTime()));
+        return calobj;
     }
 }
