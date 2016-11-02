5921a31c778e2b109213cc73f738c1ebd4c862d0
mariahbozeman <boz09001@byui.edu>
2016-11-02 09:24:08 -0600

    changed Mike's comment

--- app/src/main/java/com/example/carapp/MainActivity.java
+++ app/src/main/java/com/example/carapp/MainActivity.java
@@ -65,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             TextView theTextView = (TextView)findViewById(R.id.textView);
-            theTextView.setText("You Need Oil!");
+            theTextView.setText("You Need Tires!");
             //Toast.makeText(MainActivity.this, v, Toast.LENGTH_LONG).show();
         }
     };
