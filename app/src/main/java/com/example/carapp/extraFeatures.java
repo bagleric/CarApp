d9e037ebe9233fc14eb1b179f03cfc862d926bfb
rol09003 <rol09003@byui.edu>
2016-11-14 10:01:29 -0700

    add months

--- app/src/main/java/com/example/carapp/extraFeatures.java
+++ app/src/main/java/com/example/carapp/extraFeatures.java
@@ -5,80 +5,95 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by Mike on 11/11/2016.
  */
 
 public class extraFeatures extends AppCompatActivity {
-    String nameSpecialRequest;
-    String miles;
-    String Odometer;
+//    String nameSpecialRequest;
+//    String miles;
+//    String Odometer;
     DateFormat dateFormat = new SimpleDateFormat("MM/dd/yy");
     @Override
     protected void onCreate(Bundle savedExtraFeaturesState) {
         super.onCreate(savedExtraFeaturesState);
         setContentView(R.layout.extra_features);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();  // Always call the superclass method first
         Button buttonTire = (Button) findViewById(R.id.button);
         buttonTire.setOnClickListener(submitListener);
     }
+//
+//    @Override
+//    public void onResume() {
+//        super.onResume();  // Always call the superclass method first
+//        Button buttonTire = (Button) findViewById(R.id.button);
+//        buttonTire.setOnClickListener(submitListener);
+//    }
 
     private View.OnClickListener submitListener= new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             Button button = (Button)findViewById(R.id.button);
-
-            EditText _nameSpecialR = (EditText) findViewById(R.id.name);
-            setNameSR( _nameSpecialR.getText().toString());
-
-            EditText setOd = (EditText) findViewById(R.id.Odometer);
-            setOdometer( setOd.getText().toString());
+// we need this just commented out for testing
+//            EditText _nameSpecialR = (EditText) findViewById(R.id.name);
+//            setNameSR( _nameSpecialR.getText().toString());
+//
+//            EditText setOd = (EditText) findViewById(R.id.Odometer);
+//            setOdometer( setOd.getText().toString());
+//
+//
+//            EditText setM = (EditText) findViewById(R.id.milesTill);
+//            setMiles( setM.getText().toString());
 
             EditText MnthsTill = (EditText) findViewById(R.id.monthsTill);
-            MonthsTill( MnthsTill.getText().toString());
+            int num = Integer.valueOf(MnthsTill.getText().toString());
+            MonthsTill(num);
 
-            EditText setM = (EditText) findViewById(R.id.milesTill);
-            setMiles( setM.getText().toString());
 
         }
     };
 
-    public String nameSpecialRequest() {
-        return nameSpecialRequest;
-    }
+//    public String nameSpecialRequest() {
+//        return nameSpecialRequest;
+//    }
+//
+//    public String miles() {
+//        return miles;
+//    }
+//
+//    public String Odometer() {
+//        return Odometer;
+//    }
+//
+//    public DateFormat date(){return dateFormat;}
+//
+//    public void setNameSR(String _nameSpecialRequest) {
+//        this.nameSpecialRequest = _nameSpecialRequest;
+//    }
+//
+//    public void setMiles(String _miles) {
+//        this.miles = _miles;
+//    }
+//
+//    public void setOdometer(String _Odometer) {
+//        this.Odometer = _Odometer;
+//    }
 
-    public String miles() {
-        return miles;
-    }
+   // public void setDate(DateFormat dateFormat);
 
-    public String Odometer() {
-        return Odometer;
-    }
-
-    public DateFormat date(){return dateFormat;}
-
-    public void setNameSR(String _nameSpecialRequest) {
-        this.nameSpecialRequest = _nameSpecialRequest;
-    }
-
-    public void setMiles(String _miles) {
-        this.miles = _miles;
-    }
-
-    public void setOdometer(String _Odometer) {
-        this.Odometer = _Odometer;
-    }
-
-    public void MonthsTill(String _dateFormat) {
-        //this.dateFormat = _dateFormat;
+    public void MonthsTill(int _userDate) {
+        String serviceDate;
+        Calendar currentDate = Calendar.getInstance();
+        currentDate.setTime(new Date()); // Now use today date.
+        currentDate.add(Calendar.MONTH, _userDate); // Adds 15 months
+        serviceDate = dateFormat.format(currentDate.getTime());
+        Toast.makeText(extraFeatures.this, serviceDate, Toast.LENGTH_LONG).show();
     }
 
 
