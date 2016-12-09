87ed782b7e23d4a6b3dd5f3fc2fdfc5ec037512d
rol09003 <rol09003@byui.edu>
2016-12-08 21:03:14 -0700

    editing fix

--- app/src/main/java/com/example/carapp/MainActivity.java
+++ app/src/main/java/com/example/carapp/MainActivity.java
@@ -10,7 +10,9 @@ import android.view.View;
 import android.widget.Button;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,53 +27,38 @@ public class MainActivity extends AppCompatActivity {
 
 
     List<node> NodeArray = new ArrayList<node>();
+    public static final String ARRAY_NAME = "array_location";
 
-    public List<node> addToNodeArrayAndSort(node ThisNodeObject)
-    {
-
-        Log.d(TAG, "trying to add to array");
-        node methodCaller = new node();
-        if(NodeArray.size() < 1) {
-            NodeArray.add(ThisNodeObject);
-          //  Log.d("Size of array =====", String.valueOf(NodeArray.size()));
-        }
-        else{
-            NodeArray.add(ThisNodeObject);
-          //  Log.d("new size of array =====", String.valueOf(NodeArray.size()));
-        }
-        if(NodeArray.size() > 1) {
-         //   Log.d("Size 0 why am I here?!", String.valueOf(NodeArray.size()));
-            for (int j = 0; j < NodeArray.size(); j++) {
-                if (methodCaller.isDateOneLaterThanDateTwo(NodeArray.get(j), NodeArray.get(j + 1))) {
-                    node temp = NodeArray.get(j);
-                    NodeArray.set(j, NodeArray.get(j + 1));
-                    j++;
-                    NodeArray.set(j, temp);
-                    j = 0;
-                }
-            }
-        }
-        Log.d("new size of array =====", String.valueOf(NodeArray.size()));
-        for(int k = 0; k < NodeArray.size(); k++)
-            Log.d("new size of array =====", NodeArray.get(k).toString());
-
-        SharedPreferences.Editor editor = getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
-        Gson gson = new Gson();
-        String json = gson.toJson(NodeArray);
-        editor.putString(TAG, json);
-        editor.commit();
-        return NodeArray;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("new size of array =====", String.valueOf(NodeArray.size()));
-        for(int k = 0; k < NodeArray.size(); k++)
-            Log.d("new size of array =====", NodeArray.get(k).toString());
+        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
 
+        Gson gson = new Gson();
+        String json = preferences.getString(ARRAY_NAME, "");
+        Log.i("Calendar", json);
+        if (json.length() >= 5)
+            json = json.substring(5);
+
+        Type type = new TypeToken<ArrayList<node>>() {}.getType();
+        NodeArray = gson.fromJson(json, type);
+
+        Button buttonTire = (Button) findViewById(R.id.tireButton);
+        Button buttonOil = (Button) findViewById(R.id.oilButton);
+
+        for (int i = 0; i < NodeArray.size(); i++){
+                if(NodeArray.get(i).getNameSpecialRequest().equals("Oil Change"))
+                {
+                    buttonOil.setText(NodeArray.get(i).getDateInStringFormat() + " " + NodeArray.get(i).getNameSpecialRequest());
+                }
+            if(NodeArray.get(i).getNameSpecialRequest().equals("Tire Change"))
+            {
+                buttonTire.setText(NodeArray.get(i).getDateInStringFormat() + " " + NodeArray.get(i).getNameSpecialRequest());
+            }
+        }
     }
 
     @Override
